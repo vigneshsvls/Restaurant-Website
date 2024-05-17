34a755f81482f782fd7c144cdffc59ec6b43bc4c
vigneshsvls <136967227+vigneshsvls@users.noreply.github.com>
2024-05-17 21:57:09 +0530

    Add files via upload

---
+++ itemcounter.js
@@ -0,0 +1,77 @@
+let count=0;
+var res=0;
+document.getElementById("atc").onclick=function(){
+    count=count+1;
+    var cost1=200;
+    res=res+cost1;
+    document.getElementById("countlabel").innerHTML=count;
+    document.getElementById("totres").innerHTML=res;
+}
+document.getElementById("atc1").onclick=function(){
+    count=count+1;
+    var cost1=300;
+    res=res+cost1;
+    document.getElementById("countlabel").innerHTML=count;
+    document.getElementById("totres").innerHTML=res;
+}
+document.getElementById("atc2").onclick=function(){
+    count=count+1;
+    var cost1=220;
+    res=res+cost1;
+    document.getElementById("countlabel").innerHTML=count;
+    document.getElementById("totres").innerHTML=res;
+}
+document.getElementById("atc3").onclick=function(){
+    count=count+1;
+    var cost1=180;
+    res=res+cost1;
+    document.getElementById("countlabel").innerHTML=count;
+    document.getElementById("totres").innerHTML=res;
+}
+
+document.getElementById("atc4").onclick=function(){
+    count=count+1;
+    var cost1=250;
+    res=res+cost1;
+    document.getElementById("countlabel").innerHTML=count;
+    document.getElementById("totres").innerHTML=res;
+}
+
+document.getElementById("atc5").onclick=function(){
+    count=count+1;
+    var cost1=380;
+    res=res+cost1;
+    document.getElementById("countlabel").innerHTML=count;
+    document.getElementById("totres").innerHTML=res;
+}
+
+document.getElementById("atc6").onclick=function(){
+    count=count+1;
+    var cost1=420;
+    res=res+cost1;
+    document.getElementById("countlabel").innerHTML=count;
+    document.getElementById("totres").innerHTML=res;
+}
+
+document.getElementById("atc7").onclick=function(){
+    count=count+1;
+    var cost1=400;
+    res=res+cost1;
+    document.getElementById("countlabel").innerHTML=count;
+    document.getElementById("totres").innerHTML=res;
+}
+/*document.getElementById("atc").onclick=function(){
+    count=count-1;
+    document.getElementById("countlabel").innerHTML=count;
+}
+
+function calculation(){
+    let value1=document.getElementById("countlabel").innerHTML;
+    let total=value1*200;
+
+    console.log("Total: "+total);
+    document.getElementById("productValueDisplay").innerHTML = "Product value: " + productValue;
+    window.location.href = "page2.html?productValue=" + productValue;
+
+    return total;
+}*/
